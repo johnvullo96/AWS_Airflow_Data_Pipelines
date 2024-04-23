2213da35c6d4d664c709b0c2e6229bdd49e9e2b6
John Vullo <89565754+johnvullo96@users.noreply.github.com>
2024-04-22 23:35:29 -0700

    Update final_project.py

--- dags/final_project.py
+++ dags/final_project.py
@@ -10,13 +10,20 @@ from helpers import SqlQueries
 default_args = {
     'owner': 'udacity',
     'start_date': pendulum.now(),
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5),
+    'catchup': False,
+    'email': {
+        'on_retry': False
+    },
 }
 
 @dag(
     default_args=default_args,
     description='Load and transform data in Redshift with Airflow',
-    schedule_interval='0 * * * *'
+    schedule_interval='@daily'
 )
+
 def final_project():
 
     start_operator = DummyOperator(task_id='Begin_execution')
@@ -52,5 +59,24 @@ def final_project():
     run_quality_checks = DataQualityOperator(
         task_id='Run_data_quality_checks',
     )
+
+
+    create_schema >> start_operator
+    start_operator >> stage_events_to_redshift
+    start_operator >> stage_songs_to_redshift
+
+    stage_events_to_redshift >> load_songplays_table
+    stage_songs_to_redshift >> load_songplays_table
+
+    load_songplays_table >> load_song_dimension_table
+    load_songplays_table >> load_user_dimension_table
+    load_songplays_table >> load_artist_dimension_table
+    load_songplays_table >> load_time_dimension_table
+
+    load_song_dimension_table >> run_quality_checks
+    load_user_dimension_table >> run_quality_checks
+    load_artist_dimension_table >> run_quality_checks
+    load_time_dimension_table >> run_quality_checks
+    run_quality_checks >> end_operator
 
 final_project_dag = final_project()
